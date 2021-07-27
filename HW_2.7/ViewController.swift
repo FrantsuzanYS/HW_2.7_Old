a7b4970ba76ff4337a558f1a754b4421a66b960e
Frantsuzan_YS <niajulya@icloud.com>
2021-07-27 17:48:54 +0300

    Initial Commit

---
+++ HW_2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.7
+//
+//  Created by emmisar on 27.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
